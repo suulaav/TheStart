2795f97e0a244be7e396ecbae78e6e88b588b445
Sulav <suulaav@gmail.com>
2023-10-22 22:17:18 +0545

    base setup

--- start.sh
+++ start.sh
@@ -1,11 +1,16 @@
-cd /opt || { echo "Cannot change dir to opt";  exit  1; }
-wait
 mkdir -p "start/db/mongodb_data" || { echo "Error making folder";  exit  1; }
 cd start || { echo "Error during folder creation";  exit  1; }
-git clone https://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@github.com/suulaav/TheStart.git
+git clone https://github.com/suulaav/TheStart.git
 wait
-cd TheStart || { echo "empty-bottle folder found error during git clone";  exit  1; }
+cd TheStart || { echo "TheStart folder not found, error during git clone";  exit  1; }
+wait
+cd backend || { echo "backend folder not found, error during git clone";  exit  1; }
+wait
 mvn clean install
 wait
+cd ..
+wait
+#cd frontend || { echo "frontend folder not found, error during git clone";  exit  1; }
+#wait
 docker-compose up -d --build
-waitdocke
+wait
