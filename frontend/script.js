c3c6d84d06903845410ef41078b5739dac985a73
Sulav <suulaav@gmail.com>
2023-10-22 23:39:44 +0545

    base setup

---
+++ frontend/script.js
@@ -0,0 +1,35 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const itemForm = document.getElementById("itemForm");
+    const responseDiv = document.getElementById("response");
+
+    itemForm.addEventListener("submit", function (event) {
+        event.preventDefault();
+
+        const id = document.getElementById("id").value;
+        const name = document.getElementById("name").value;
+        const quantity = parseInt(document.getElementById("quantity").value);
+        const category = document.getElementById("category").value;
+
+        const itemData = {
+            id: id,
+            name: name,
+            quantity: quantity,
+            category: category,
+        };
+
+        fetch("http://0.0.0.0/backend", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(itemData),
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                responseDiv.textContent = "POST request successful. Response: " + JSON.stringify(data);
+            })
+            .catch((error) => {
+                responseDiv.textContent = "Error: " + error;
+            });
+    });
+});
