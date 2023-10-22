4051d61f2a0b899963c3e4f5575097cf1a00bbfc
Sulav <suulaav@gmail.com>
2023-10-23 00:32:42 +0545

    base setup

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
-mkdir -p "start/db/mongodb_data" || { echo "Error making folder";  exit  1; }
+mkdir -p "db/mongodb_data" || { echo "Error making folder";  exit  1; }
+mkdir -p "start" || { echo "Error making folder";  exit  1; }
 cd start || { echo "Error during folder creation";  exit  1; }
 git clone https://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@github.com/suulaav/TheStart.git
 wait
